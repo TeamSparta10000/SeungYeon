4a7031b7889a50860b8fb4cbe328f8496bf7cdb3
SeungYeon04 <100332811+SeungYeon04@users.noreply.github.com>
2023-11-21 20:41:29 +0900

    Create program.cs

---
+++ 응애/program.cs
@@ -0,0 +1,54 @@
+using Dungeon_Adventure;
+namespace Dungeon_Adventure
+{
+    public class Program
+    {
+        static void Main()
+        {
+            GameData.GameDataSetting();
+            Scene.DisplayGameStart();
+            Scene.DisplayTown();
+            Scene.End();
+            Scene.End2();
+        }
+
+        public static void ToggleEqupStatus(int idx)
+        {
+            GameData.items[idx].IsEquipped = !GameData.items[idx].IsEquipped;
+        }
+
+        public static void ShowHighlightText(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+        public static void PrintTextWithHighlights(string s1, string s2, string s3 = "")
+        {
+            Console.Write(s1);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(s2);
+            Console.ResetColor();
+            Console.WriteLine(s3);
+        }
+        public static int CheckValidInput(int min, int max)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                bool parseSuccess = int.TryParse(input, out var ret);
+                if (parseSuccess)
+                {
+                    if (ret >= min && ret <= max)
+                        return ret;
+                }
+
+                Console.WriteLine("잘못된 입력입니다.");
+            }
+        }
+
+    }
+
+
+}
